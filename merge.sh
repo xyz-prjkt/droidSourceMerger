78ac95fb2ece0d1c1648c3d9aced1c9d1fc8e41c
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-16 01:58:31 +0700

    xd-merger: merge `LA.QSSI.11.0.r1-12600-qssi.0`

--- merge.sh
+++ merge.sh
@@ -6,7 +6,7 @@
 XDROID_VERSION=rev1.0
 XDROID_CODENAME=Redstone
 XDROID_REVISION=xd_${XDROID_VERSION}.${XDROID_CODENAME}
-CAF_TAG=LA.QSSI.11.0.r1-12400.02-qssi.0
+CAF_TAG=LA.QSSI.11.0.r1-12600-qssi.0
 SOURCE=https://github.com/xdroid-CAF
 
 # Inlined function to post a message
@@ -23,85 +23,97 @@ tg_post_msg "<b>xdroid Source Updater</b>%0A<b>xdroid Revision</b>: <code>${XDRO
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/build_make" $(pwd)/build/make
 cd $(pwd)/build/make
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/build && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/build/make
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/build_blueprint" $(pwd)/build/blueprint
 cd $(pwd)/build/blueprint
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/build/blueprint && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/build/blueprint
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/build_soong" $(pwd)/build/soong
 cd $(pwd)/build/soong
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/build/soong && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/build/soong
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/frameworks_av" $(pwd)/frameworks/av
 cd $(pwd)/frameworks/av
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/frameworks/av && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/frameworks/av
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/frameworks_base" $(pwd)/frameworks/base
 cd $(pwd)/frameworks/base
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/frameworks/base && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/frameworks/base
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/frameworks_native" $(pwd)/frameworks/native
 cd $(pwd)/frameworks/native
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/frameworks/native && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/frameworks/native
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/hardware_interfaces" $(pwd)/hardware/interfaces
 cd $(pwd)/hardware/interfaces
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/hardware/interfaces && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/hardware/interfaces
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/packages_apps_Bluetooth" $(pwd)/packages/apps/Bluetooth
 cd $(pwd)/packages/apps/Bluetooth
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/packages/apps/Bluetooth && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../../..
 rm -rf $(pwd)/packages/apps/Bluetooth
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/packages_apps_Settings" $(pwd)/packages/apps/Settings
 cd $(pwd)/packages/apps/Settings
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/packages/apps/Settings && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../../..
 rm -rf $(pwd)/packages/apps/Settings
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/system_core" $(pwd)/system/core
 cd $(pwd)/system/core
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/system/core && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../..
 rm -rf $(pwd)/system/core
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/vendor_qcom_opensource_commonsys_system_bt" $(pwd)/vendor/qcom/opensource/commonsys/system/bt
 cd $(pwd)/vendor/system/bt
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/system/bt && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../../..
 rm -rf $(pwd)/vendor/system/bt
 
 git clone "https://$GH_USERNAME:$GH_TOKEN@github.com/xdroid-CAF/vendor_qcom_opensource_commonsys_packages_apps_Bluetooth" $(pwd)/vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 cd $(pwd)/vendor/apps/Bluetooth
+git checkout -b staging-${CAF_TAG}
 git remote add caf https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/apps/Bluetooth && git fetch caf ${CAF_TAG} && git merge FETCH_HEAD
-git push
+git push -f origin staging-${CAF_TAG}
 cd ../../..
 rm -rf $(pwd)/vendor/apps/Bluetooth
 
