8141456d4456652b707573c8417597777caaba57
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-10-06 17:09:03 +0700

    xd-merger: merge `LA.QSSI.11.0.r1-13500-qssi.0`

--- merge.sh
+++ merge.sh
@@ -3,10 +3,10 @@
 # Copyright (C) 2021 a xyzprjkt property
 #
 
-XDROID_VERSION=rev1.5.5
+XDROID_VERSION=rev1.6
 XDROID_CODENAME=redwhite
 XDROID_REVISION=xd_${XDROID_VERSION}.${XDROID_CODENAME}
-CAF_TAG=LA.QSSI.11.0.r1-13300-qssi.0
+CAF_TAG=LA.QSSI.11.0.r1-13500-qssi.0
 SOURCE=https://github.com/xdroid-CAF
 
 # Inlined function to post a message
