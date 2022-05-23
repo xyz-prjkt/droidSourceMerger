80c3aeb1e1d73faed97af23ba18287c80176a7c9
Jody Yuantoro <xyzuannihboss@gmail.com>
2022-05-23 20:38:47 +0700

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -3,10 +3,10 @@
 # Copyright (C) 2021 a xyzprjkt property
 #
 
-XDROID_VERSION=r6
+XDROID_VERSION=r7
 XDROID_BRANCH_OLD=twelve-dev
 XDROID_SOURCE=github.com/xdroid-oss
-AOSP_TAG=android-12.1.0_r2
+AOSP_TAG=android-12.1.0_r5
 XDROID_BRANCH_NEW=twelve-${AOSP_TAG}
 # XDROID_BRANCH_NEW=INA.XD.12.0-${XDROID_VERSION}.${AOSP_TAG} // Migration Purpose
 XDROID_REVISION=${XDROID_BRANCH_NEW}
